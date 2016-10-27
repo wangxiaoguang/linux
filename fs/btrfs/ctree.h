f1070bd64289969fe875cccacc9411f8b8301c1f
Wang Xiaoguang <wangxg.fnst@cn.fujitsu.com>
2016-10-27 15:46:04 +0800

    btrfs: fix false enospc for in-band dedupe
    Signed-off-by: Wang Xiaoguang <wangxg.fnst@cn.fujitsu.com>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -107,6 +107,7 @@ static const int btrfs_csum_sizes[] = { 4 };
 enum btrfs_metadata_reserve_type {
 	BTRFS_RESERVE_NORMAL,
 	BTRFS_RESERVE_COMPRESS,
+	BTRFS_RESERVE_DEDUPE,
 };
 int inode_need_compress(struct inode *inode);
 u64 btrfs_max_extent_size(enum btrfs_metadata_reserve_type reserve_type);
